5fa597a5e42e26a577bf3be9a1d09f2f668b23f8
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-06-17 16:23:23 -0300

    fix: fix typing for journals response

--- internal/application/service/journal-service/list-user-journals-service.go
+++ internal/application/service/journal-service/list-user-journals-service.go
@@ -1,7 +1,19 @@
 package journalservice
 
-import "github.com/mauFade/journaly/internal/domain"
+import (
+	"github.com/mauFade/journaly/internal/application/dto"
+)
 
-func (s *JournalService) ListUserJournals(userId string) ([]*domain.JournalModel, error) {
-	return s.repository.GetByUser(userId)
+func (s *JournalService) ListUserJournals(userId string) ([]*dto.JournalResponse, error) {
+	js, err := s.repository.GetByUser(userId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	journals := []*dto.JournalResponse{}
+	for _, j := range js {
+		journals = append(journals, dto.ToJournalDto(j))
+	}
+	return journals, nil
 }
